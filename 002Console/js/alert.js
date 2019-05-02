50ebe72496831083b8d4ad563f28492cfd78cc1f
A556UR <15013044544@163.com>
2019-05-02 23:08:01 +0800

    modify

---
+++ 002Console/js/alert.js
@@ -0,0 +1,20 @@
+/*可以通过function定义函数来实现封装*/
+function taoLu(){
+    alert("你猜一会儿会发生什么");
+    alert("再等等");
+    alert("也许有奇迹发生");
+}
+
+alert("你怎么打开这个网页");
+taoLu();
+
+
+alert("你相信奇迹吗？");
+alert("让我来像你提个问题");
+taoLu();
+
+alert("果然没有奇迹");
+alert("你就要这样放弃吗");
+taoLu();
+
+alert("SUKI!");
